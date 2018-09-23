0882aff45d6e401ae04d0fcc6e00642720fb6b08
Mahbub Alam Evan <mahbubalamevan@Mahbubs-MacBook-Air.local>
2018-09-23 21:53:37 +0600

    Close and Delete Funtion Updated

--- main.js
+++ main.js
@@ -33,6 +33,32 @@ function saveIssue(e){
 
 }
 
+function setStatusClosed(id){
+  var issues = JSON.parse(localStorage.getItem('issues'));
+
+  for(var i=0;i<issues.length;i++){
+    if(issues[i].id == id){
+      issues[i].status = "Closed";
+    }
+  }
+
+  localStorage.setItem('issues',JSON.stringify(issues));
+  fetchIssues();
+}
+
+function deleteIssue(id){
+  var issues = JSON.parse(localStorage.getItem('issues'));
+
+  for(var i=0;i<issues.length;i++){
+    if(issues[i].id == id){
+      issues.splice(i,1);
+    }
+  }
+
+  localStorage.setItem('issues',JSON.stringify(issues));
+  fetchIssues();
+}
+
 function fetchIssues() {
   var issues = JSON.parse(localStorage.getItem('issues'));
   var issueList = document.getElementById('issuesList');
